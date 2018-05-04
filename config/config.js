658b160eb2ce54112bd3d21a02a2028ba775c308
Antonio <jrobles.antonio@gmail.com>
2018-05-04 01:13:34 -0700

    More edits.

--- config/config.js
+++ config/config.js
@@ -9,13 +9,13 @@
  */
 
 var config = {
-	address: "localhost", // Address to listen on, can be:
+	address: "0.0.0.0", // Address to listen on, can be:
 	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 	                      // - another specific IPv4/6 to listen on a specific interface
 	                      // - "", "0.0.0.0", "::" to listen on any interface
 	                      // Default, when address config is left out, is "localhost"
-	port: 8080,
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+	port: 8000,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", '192.168.1.0/24', "::ffff:192.168.1.0/24"], // Set [] to allow all IP addresses
 	                                                       // or add a specific IPv4 of 192.168.1.5 :
 	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -31,7 +31,7 @@ var config = {
 		},
 		{
 			module: "updatenotification",
-			position: "top_bar"
+			position: "top_right"
 		},
 		{
 			module: "clock",
@@ -48,7 +48,7 @@ var config = {
 			config: {
 				alarms: [
 					{ time: "23:30", days: [1,3], title: "Class", message: "Time to head out!" },
-					{ time: "07:00", days: [1,2,3,4,5], title: "Work", message: "Don't be Late!" }
+					{ time: "07:00", days: [1,2,3,4,5,6], title: "Work", message: "Don't be Late!" }
 				],
 				touch: true
 			}
@@ -115,6 +115,9 @@ var config = {
 				updateInterval: 30000,
 			}
 		},
+		{
+			"module": "MMM-Admin-Interface"
+		}
 	]
 
 };
